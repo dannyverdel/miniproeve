67605c87738563a30c9b6f34fcbcbd14eaf780c1
dannyverdel <danny.verdel@gmail.com>
2021-09-01 14:43:27 +0200

    begonnen aan bestelpagina

--- server/validation.js
+++ server/validation.js
@@ -20,5 +20,14 @@ const question_validation = (obj) => {
   return Joi.validate(obj, schema);
 };
 
+const category_validation = (obj) => {
+  const schema = {
+    category: Joi.string().required(),
+  };
+
+  return Joi.validate(obj, schema);
+};
+
 module.exports.dish_validation = dish_validation;
 module.exports.question_validation = question_validation;
+module.exports.category_validation = category_validation;
